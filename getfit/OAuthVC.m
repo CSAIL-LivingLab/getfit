46399c69e52661ef11c3590c2bd6d2054b5d7e91
Albert Carter <albert.r.carter@gmail.com>
2015-01-06 12:54:56 -0500

    fix javascript

--- getfit/OAuthVC.m
+++ getfit/OAuthVC.m
@@ -96,14 +96,14 @@
 - (void) extractTokens {
     NSLog(@"extract tokens hit");
     
-    NSString * jsCallBack = [webView stringByEvaluatingJavaScriptFromString:@"var index; var arr = document.getElementsByName('form_id'); for (var i = 0; i < arr.length; i++) {    if ('getfit_minutes_single_form_2' == arr[i].value) {        index = i;    };} var form_token = document.getElementsByName('form_token')[index].value; var form_build_id = document.getElementsByName('form_build_id')[index].value; var form_id = 'getfit_minutes_single_form_2'; function foo() { return form_token+','+form_build_id+','+form_id; } foo();"];
-    NSString *indexStr = [webView stringByEvaluatingJavaScriptFromString:jsCallBack];
+    NSString * tokenStr = [webView stringByEvaluatingJavaScriptFromString:@"var index; var arr = document.getElementsByName('form_id'); for (var i = 0; i < arr.length; i++) {    if ('getfit_minutes_single_form_2' == arr[i].value) {        index = i;    };} var form_token = document.getElementsByName('form_token')[index].value; var form_build_id = document.getElementsByName('form_build_id')[index].value; var form_id = 'getfit_minutes_single_form_2'; function foo() { return form_token+','+form_build_id+','+form_id; } foo();"];
     
-    NSLog(@"%@", indexStr);
-
-
+    // parse tokens
+    NSArray *tokens = [tokenStr componentsSeparatedByString:@","];
     
-    
+    NSString *form_token = tokens[0];
+    NSString *form_build_id = tokens[1];
+    NSString *form_id = tokens[2];
     
     
 }
