e2a01cf902488e0920a61e7bbaf85d63666a485c
Albert Carter <albert.r.carter@gmail.com>
2015-01-06 13:29:25 -0500

    finish web scraping. Now working on integrating tokens

--- getfit/OAuthVC.m
+++ getfit/OAuthVC.m
@@ -13,10 +13,10 @@
 @end
 
 @implementation OAuthVC {
-    UIWebView *webView;
-    NSString *urlStr;
+    UIWebView *myWebView;
     NSString *email;
     NSString *token;
+    NSUserDefaults *defaults;
 }
 
 - (void)viewDidLoad {
@@ -24,13 +24,13 @@
     
     // make a bar button
     [self makeDoneButton];
-    [self makeScrapeButton];
-    
-    // prep the url
-    [self prepUrl];
     
     // make the web view load
     [self setupWebView];
+    
+    // prep defaults
+    defaults = [NSUserDefaults standardUserDefaults];
+    
 }
 
 
@@ -44,42 +44,17 @@
     self.navigationItem.rightBarButtonItem = rightButton;
 }
 
-- (void) makeScrapeButton {
-    UIBarButtonItem *leftButton = [[UIBarButtonItem alloc] initWithTitle:@"Scrape"
-                                                                   style:UIBarButtonSystemItemAction target:self action:@selector(scrape)];
-    self.navigationItem.leftBarButtonItem = leftButton;
-}
-
 - (void) setupWebView {
     // layout view
     CGRect screenRect = [[UIScreen mainScreen] bounds];
-    webView=[[UIWebView alloc]initWithFrame:CGRectMake(0, 0, screenRect.size.width ,screenRect.size.height)];
-    webView.delegate = self;
+    myWebView=[[UIWebView alloc]initWithFrame:CGRectMake(0, 0, screenRect.size.width ,screenRect.size.height)];
+    myWebView.delegate = self;
     
     // load url
-    NSURL *nsurl=[NSURL URLWithString:urlStr];
+    NSURL *nsurl=[NSURL URLWithString:@"https://getfit-d7-dev.mit.edu/Shibboleth.sso/Login?target=https%3A%2F%2Fgetfit-d7-dev.mit.edu%2F%3Fq%3Dshib_login%2Ffront-page"];
     NSURLRequest *nsrequest=[NSURLRequest requestWithURL:nsurl];
-    [webView loadRequest:nsrequest];
-    [self.view addSubview:webView];
-
-}
-
-- (void) prepUrl{
-    // this method exists because I may need to do some more compliated things with urls alter.
-    urlStr = @"https://getfit-d7-dev.mit.edu/Shibboleth.sso/Login?target=https%3A%2F%2Fgetfit-d7-dev.mit.edu%2F%3Fq%3Dshib_login%2Ffront-page";
-}
-
-// extract email address
-// extract tokens
-
-- (void) scrape {
-    NSString *url = [webView stringByEvaluatingJavaScriptFromString:@"document.URL"];
-    
-    if ([url rangeOfString:@"profile"].location != NSNotFound) {
-        [self extractEmail];
-    } else if ([url rangeOfString:@"dashboard"].location != NSNotFound) {
-        [self extractTokens];
-    }
+    [myWebView loadRequest:nsrequest];
+    [self.view addSubview:myWebView];
 
 }
 
@@ -87,34 +62,51 @@
     NSLog(@"extract Email hit");
     
     NSString * jsCallBack = [NSString stringWithFormat:@"document.getElementById(\"edit-mail\").value"];
-    email = [webView stringByEvaluatingJavaScriptFromString:jsCallBack];
-    NSLog(@"%@", email);
-    
-    
+    email = [myWebView stringByEvaluatingJavaScriptFromString:jsCallBack];
+    [defaults setObject:email forKey:@"email"];
 }
 
 - (void) extractTokens {
     NSLog(@"extract tokens hit");
     
-    NSString * tokenStr = [webView stringByEvaluatingJavaScriptFromString:@"var index; var arr = document.getElementsByName('form_id'); for (var i = 0; i < arr.length; i++) {    if ('getfit_minutes_single_form_2' == arr[i].value) {        index = i;    };} var form_token = document.getElementsByName('form_token')[index].value; var form_build_id = document.getElementsByName('form_build_id')[index].value; var form_id = 'getfit_minutes_single_form_2'; function foo() { return form_token+','+form_build_id+','+form_id; } foo();"];
+    NSString * tokenStr = [myWebView stringByEvaluatingJavaScriptFromString:@"var index; var arr = document.getElementsByName('form_id'); for (var i = 0; i < arr.length; i++) {    if ('getfit_minutes_single_form_2' == arr[i].value) {        index = i;    };} var form_token = document.getElementsByName('form_token')[index].value; var form_build_id = document.getElementsByName('form_build_id')[index].value; var form_id = 'getfit_minutes_single_form_2'; function foo() { return form_token+','+form_build_id+','+form_id; } foo();"];
     
     // parse tokens
     NSArray *tokens = [tokenStr componentsSeparatedByString:@","];
-    
     NSString *form_token = tokens[0];
     NSString *form_build_id = tokens[1];
     NSString *form_id = tokens[2];
     
+    // set save as defaults.
+    [defaults setObject:form_token forKey:@"form_token"];
+    [defaults setObject:form_build_id forKey:@"form_build_id"];
+    [defaults setObject:form_id forKey:@"form_id"];
     
+    // once tokens are extracted, extrac the user email
+    NSURL *profileURL =[NSURL URLWithString:@"https://getfit-d7-dev.mit.edu/profile"];
+    NSURLRequest *nsrequest=[NSURLRequest requestWithURL:profileURL];
+    [myWebView loadRequest:nsrequest];
 }
 
 - (void) webViewDidFinishLoad:(UIWebView *)webView
 {
     NSString *url = [webView stringByEvaluatingJavaScriptFromString:@"document.URL"];
-//    [self scrape];
-    // hide window
-    // extract email address
-    // close window
+    NSLog(@"------\n%@\n------", url);
+    
+    // do nothing unless it's a getfit url
+    if ([url rangeOfString:@"getfit"].location == NSNotFound || [url rangeOfString:@"idp.mit.edu"].location != NSNotFound) {
+        return;
+    }
+    
+    // should hide the webView and do this all automatically.
+    if ([url rangeOfString:@"profile"].location != NSNotFound) {
+        [self extractEmail];
+    } else if ([url rangeOfString:@"dashboard"].location != NSNotFound) {
+        [self extractTokens];
+    }
+
+
+
     
 }
 
