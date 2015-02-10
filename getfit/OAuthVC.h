71d1d48dbddabdaf3d1c09cb8d893fc1db29111f
Albert Carter <albert.r.carter@gmail.com>
2015-02-10 13:39:02 -0500

    add comments for happy and sad path in OAuthVC

--- getfit/OAuthVC.h
+++ getfit/OAuthVC.h
@@ -26,3 +26,51 @@
 @property (strong, atomic) UIViewController<OAuthVCDelegate> *delegate;
 
 @end
+
+/* HAPPY PATH FOR URLS:
+ 
+ START:
+ about:blank
+ 
+ FINISH:
+ https://wayf.mit.edu/WAYF?shire=https%3A%2F%2Fgetfit.mit.edu%2FShibboleth.sso%2FSAML%2FPOST&time=1423535993&target=cookie%3A62c4d9f7&providerId=https%3A%2F%2Fgetfit.mit.edu%2Fshibboleth
+ 
+ 
+ START:
+ https://wayf.mit.edu/WAYF?shire=https%3A%2F%2Fgetfit.mit.edu%2FShibboleth.sso%2FSAML%2FPOST&time=1423535993&target=cookie%3A62c4d9f7&providerId=https%3A%2F%2Fgetfit.mit.edu%2Fshibboleth
+ 
+ FINISH:
+ https://idp.mit.edu/idp/Authn/MIT
+ 
+ START:
+ https://idp.mit.edu/idp/Authn/MIT
+ 
+ FINISH:
+ https://idp.mit.edu/idp/profile/Shibboleth/SSO
+ 
+ START:
+ https://idp.mit.edu/idp/profile/Shibboleth/SSO
+ 
+ FINISH:
+ https://getfit.mit.edu/?q=dashboard
+ */
+
+/* SAD PATH FOR URLS
+ 
+ FINISH:
+ https://wayf.mit.edu/WAYF?shire=https%3A%2F%2Fgetfit.mit.edu%2FShibboleth.sso%2FSAML%2FPOST&time=1423536175&target=cookie%3A2d511812&providerId=https%3A%2F%2Fgetfit.mit.edu%2Fshibboleth
+ 
+ START:
+ https://wayf.mit.edu/WAYF?shire=https%3A%2F%2Fgetfit.mit.edu%2FShibboleth.sso%2FSAML%2FPOST&time=1423536175&target=cookie%3A2d511812&providerId=https%3A%2F%2Fgetfit.mit.edu%2Fshibboleth
+ 
+ FINISH:
+ https://idp.mit.edu/idp/Authn/MIT
+ 
+ START:
+ https://idp.mit.edu/idp/Authn/MIT
+ 
+ FINISH:
+ https://idp.mit.edu/idp/Authn/UsernamePassword
+ 
+ 
+ */
