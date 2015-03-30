93ec3eecac450c0c8fa2b5bfa65a114172101789
Albert Carter <albert.r.carter@gmail.com>
2015-03-30 17:53:17 -0400

    make activity step counter less accurate, but faster

--- OpenSense/OpenSense/Probes/OSActivityManagerProbe.m
+++ OpenSense/OpenSense/Probes/OSActivityManagerProbe.m
@@ -99,11 +99,18 @@
     CMStepCounter *sc = [[CMStepCounter alloc] init];
     
     // declare some variables accessible outside of the block
-    __block NSDictionary *point = [NSDictionary alloc];
-    __block NSMutableDictionary *data = [[NSMutableDictionary alloc] init];
-    __block NSMutableArray *arr = [[NSMutableArray alloc] init];
-    __block BOOL done = FALSE;
+    __block NSDictionary *activityPoint = [NSDictionary alloc];
+    __block NSDictionary *stepPoint = [NSDictionary alloc];
     
+    __block NSMutableDictionary *data = [[NSMutableDictionary alloc] init];
+    __block NSMutableArray *activityArr = [[NSMutableArray alloc] init];
+    
+    __block NSMutableArray *stepArr = [[NSMutableArray alloc] init];
+    
+    __block BOOL actDone = FALSE;
+    __block BOOL stepDone = FALSE;
+    
+    // activityLog block
     [cm queryActivityStartingFromDate:lastSampleDate toDate:now toQueue:activityQueue withHandler:^(NSArray *activities, NSError *error){
         // for each returned activity
         for(int i=0;i<[activities count];i++) {
@@ -117,7 +124,12 @@
             } else {
                 endDate = [NSDate date];
             }
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+            [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss:SSS"];
+            NSString *startStr = [dateFormatter stringFromDate:startDate];
+            NSString *endStr = [dateFormatter stringFromDate:endDate];
             
+            // define activity
             NSString *activityString = @"unkn";
             if (a.running) activityString = @"runn";
             else if (a.walking) activityString = @"walk";
@@ -126,51 +138,60 @@
             else if (a.cycling) activityString = @"cycl";
 
             
+            // define confidence
             NSString *confidenceString = @"low";
             if (a.confidence == CMMotionActivityConfidenceMedium) confidenceString = @"med";
             else if (a.confidence == CMMotionActivityConfidenceHigh) confidenceString = @"hig";
-//            NSLog(@"\n---Activity: %@", activityString);
-//            NSLog(@"Confidence: %@", confidenceString);
+
+            // create data point
+            activityPoint = [[NSDictionary alloc] initWithObjectsAndKeys:
+                    activityString, @"act",
+                    confidenceString, @"conf",
+                    startStr, @"start",
+                     endStr, @"end",
+                     nil];
             
-            __block BOOL actDone = FALSE;
-            // find stepCounts for that activity
-            [sc queryStepCountStartingFrom:startDate to:endDate toQueue:activityQueue withHandler:^(NSInteger numberOfSteps, NSError *error) {
-                
-                
-                NSNumber *steps = [[NSNumber alloc] initWithInteger:numberOfSteps];
-                
-                NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-                [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss:SSS"];
-                NSString *startStr = [dateFormatter stringFromDate:startDate];
-                NSString *endStr = [dateFormatter stringFromDate:endDate];
-                
-                point = [[NSDictionary alloc ] initWithObjectsAndKeys:
-                                       activityString, @"act",
-                                       confidenceString, @"conf",
-                                       steps, @"step",
-                                       startStr, @"start",
-                                       endStr, @"end",
-                                       nil];
-                
-//                NSString *activityStr = point[@"act"];
-//                NSString *confidenceStr = point[@"conf"];
-//                NSString *stepStr = [point[@"step"] stringValue];
-//                NSLog(@"\npoint activity: %@, confidence: %@, steps: %@, start: %@, end %@", activityStr, confidenceStr, stepStr, startStr, endStr);
-                
-                [arr addObject:point];
-                actDone = TRUE;
-            }];
-            while (!actDone);
-
-            [data setObject:arr forKey:@"activityLog"];
+            // add point to array
+            [activityArr addObject:activityPoint];
         }
-        done = TRUE;
         
+        // set the data to the array
+        [data setObject:activityArr forKey:@"activityLog"];
+        actDone = TRUE;
+        }];
+    
+    
+    
+    // find step count since the manager was last turned on
+    [sc queryStepCountStartingFrom:lastSampleDate to:now toQueue:activityQueue withHandler:^(NSInteger numberOfSteps, NSError *error) {
+    
+        // set up the date
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss:SSS"];
+        NSString *startStr = [dateFormatter stringFromDate:lastSampleDate];
+        NSString *endStr = [dateFormatter stringFromDate:now];
 
+        // count the steps
+        NSNumber *steps = [[NSNumber alloc] initWithInteger:numberOfSteps];
+
+        // create a point
+        stepPoint = [[NSDictionary alloc ] initWithObjectsAndKeys:
+                               steps, @"step",
+                               startStr, @"start",
+                               endStr, @"end",
+                               nil];
+    
+        // add the point to the array
+        [stepArr addObject:stepPoint];
         
-    }];
-
-    while(! done);
+        // set the data to the array
+        [data setObject:stepArr forKey:@"stepLog"];
+        stepDone = TRUE;
+        }];
+    
+    
+    // function is blocking. This will release once activities and steps are counted
+    while (!(actDone & stepDone));
     
     return data;
 }
